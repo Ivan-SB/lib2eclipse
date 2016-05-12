7920fd75a2179a3d101f9fa346cb3c8bee2d542d
Ivan Sergio Borgonovo <mail@webthatworks.it>
2016-05-12 23:48:31 +0200

    restore.sh: don't copy dir, just rename it

--- test/restore.sh
+++ test/restore.sh
@@ -1,4 +1,3 @@
 #/usr/bin/env sh
 rm -rf ~/Documents/workspace/c2eT
-cp -r ~/Documents/workspace/c2eT.bak  ~/Documents/workspace/c2eT
-rm -rf ~/Documents/workspace/c2eT.bak
+mv ~/Documents/workspace/c2eT.bak   ~/Documents/workspace/c2eT
