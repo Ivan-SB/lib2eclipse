bbac1eaf7a8642e04d3464cf33f4a711110d9114
Ivan Sergio Borgonovo <mail@webthatworks.it>
2016-05-12 23:50:22 +0200

    copy generated .cproject into c2eT testing project

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,7 @@ cp -r ~/Documents/workspace/c2eT  ~/Documents/workspace/c2eT.bak
 cd ..
 #./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.3.0 -c ~/Documents/stm32/c2eT -r -a install
 ./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.3.0 -c ~/Documents/stm32/c2eT -w -r -a install -mFreeRTOS
+cp ./.cproject ~/Documents/workspace/c2eT
 cd ./test
 
 
