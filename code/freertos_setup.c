994a72a7cdb24f8290cdc2bfbb550c72d3a543f8
Ivan Sergio Borgonovo <mail@webthatworks.it>
2016-05-18 22:24:12 +0200

    mockup for creating a main task

--- code/freertos_setup.c
+++ code/freertos_setup.c
@@ -1,9 +1,23 @@
 #include "freertos_setup.h"
 
-void MX_FREERTOS_Init(void) {
+TaskHandle_t xMainHandle;
 
+__weak void vMainTask( void *pvParameters ) {
+  for(;;) {
+
+  }
 }
 
-void HWSetup(void) {
+__weak void MX_FREERTOS_Init(void) {
+  xTaskCreate(
+      vMainTask
+      ,  (const signed portCHAR *)"Test"
+      ,  configMINIMAL_STACK_SIZE
+      ,  NULL
+      ,  tskIDLE_PRIORITY + 1
+      ,  &xMainHandle );
+}
+
+__weak void HWSetup(void) {
 
 }
