168c6aa8f5ad7160594a2244b4d734c21287a9cf
Ivan Sergio Borgonovo <mail@webthatworks.it>
2016-05-25 23:39:13 +0200

    update test to check different FreeRTOS versions

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,9 @@
 #/usr/bin/env sh
 cp -r ~/Documents/workspace/c2eT  ~/Documents/workspace/c2eT.bak
 cd ..
-#./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.3.0 -c ~/Documents/stm32/c2eT -r -a install
+#./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.4.0l -c ~/Documents/stm32/c2eT -r -a install
 ./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.3.0 -c ~/Documents/stm32/c2eT -w -r -a install -mFreeRTOS
+#./cube2eclipse.py -p ~/Documents/workspace/c2eT -l /usr/local/src/STM32Cube_FW_F1_V1.4.0l -c ~/Documents/stm32/c2eT -w -r -a install -mFreeRTOS
 cp ./.cproject ~/Documents/workspace/c2eT
 cd ./test
 
